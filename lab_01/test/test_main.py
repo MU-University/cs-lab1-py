496d0b4e5966b4cce4df5b9e6013b66d810c4419
Asliddin <98747380+eshonkulov-asliddin@users.noreply.github.com>
2024-05-14 13:44:02 +0500

    Update test_main.py

--- lab_01/test/test_main.py
+++ lab_01/test/test_main.py
@@ -1,14 +1,11 @@
 from lab_01.main.main import add
 
 
-def test_main():
-    assert 9 == add(4, 5)
-    
-def test_fail():
-    assert 9 == add(2, 4)
+def test_add_with_four_numbers():
+    assert 24 == add(4, 5, 7, 8)
 
-def test_add_with_odd_numbers():
-    assert 10 == add(7, 3)
+def test_add_with_three_numbers():
+    assert 12 == add(7, 3, 2)
 
-def test_add_with_even_numbers():
+def test_add_with_two_numbers():
     assert 20 == add(12, 8)
